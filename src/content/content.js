e44e7f87f5b318b2627833da8cb3cae742f3931c
lky1020 <57024183+lky1020@users.noreply.github.com>
2021-06-16 10:48:25 +0800

    Update Status Text
    Include little bit of Hand landmarks deviation adjustment

--- src/content/content.js
+++ src/content/content.js
@@ -44,6 +44,14 @@ const fingerJoints = {
     pinky: [0, 17, 18, 19, 20],
 };
 
+const left_deviation =  {
+    x: [230.669742, 219.801723, 201.463273, 183.3696517, 180.9756267, 210.4777548, 198.9889152, 191.2660526, 184.867725, 227.045465, 217.5762433, 
+        215.2701552,213.7340115, 239.9403986, 234.9524839, 237.6094636, 238.5510363, 249.8898322, 250.7591419, 255.2299962, 258.5984535],
+
+    y: [-222.172246, -209.9497304, -185.6502878, -174.9155134, -177.6707399, -149.6083701,-135.3207188, -131.941493, -133.4844497, -148.4339446,
+        -122.3589817, -116.925061, -108.7374331, -155.3590486, -127.8043412, -119.5259269, -107.3444485, -163.8445047, -149.4853784, -145.4639185,-143.4947074],
+};
+
 async function overrideGetUserMedia() {
     //Create canvas use for replace the Google Meet Video
     var canvas = document.createElement("canvas");
@@ -146,6 +154,7 @@ function handPoseInRealTime() {
     async function handPoseFrame() {
 
         var ctx = state.canvas.getContext("2d");
+        ctx.clearRect(0, 0, state.canvas.width, state.canvas.height);
         ctx.drawImage(state.video, 0, 0);
 
         if(Object.keys(handsfree.data).length !== 0){
@@ -153,18 +162,45 @@ function handPoseInRealTime() {
             if(handsfree.data.hands.multiHandLandmarks !== undefined && handsfree.data.hands.multiHandedness != undefined){
                 // console.log(handsfree);
                 // console.log(handsfree.data.hands.multiHandedness[0].score);
-                
+
                 var hands = handsfree.data.hands.multiHandLandmarks[0];
                 var landmarks = new Array();
-                
-                for(let i = 0; i < hands.length; i++){
-                    //Change x, y, point to 3 digit value
-                    const x_axis  = hands[i].x  * 1000;
+
+                for(let i = 0; i < 21; i++){
+                    
+                    // var deviation;
+                    // var x_axis;
+                    // var y_axis;
+
+                    // // Left Hand == Right Handness (Video Not flip in this case)
+                    // if(handsfree.data.hands.multiHandedness[0].label === "Right"){
+                    //     deviation = left_deviation;
+
+                    //     //Change x, y, point to 3 digit value 
+                    //     /*
+                    //         (Remember Bring it out)
+                    //     */
+                    //     x_axis  = hands[i].x  * 1000 + deviation.x[i];
+                    //     y_axis = hands[i].y * 1000 + deviation.y[i];
+
+                    // }else{
+                    //     //Change x, y, point to 3 digit value
+                    //     x_axis  = hands[i].x  * 1000;
+                    //     y_axis = hands[i].y * 1000;
+
+                    // }
+
+                    const  x_axis  = hands[i].x  * 1000;
                     const y_axis = hands[i].y * 1000;
 
                     var landmark = {x: x_axis, y: y_axis};
+                        
                     landmarks.push(landmark);
+
                 }
+                
+                // console.log(landmarks);
+                // console.log('Done');
 
                 // Loop through fingers (connect the point)
                 for(let i = 0; i < Object.keys(fingerJoints).length; i++){
@@ -211,38 +247,30 @@ function handPoseInRealTime() {
                         ctx.fill();
                     }
                 }
+
+                ctx.beginPath();
+                ctx.save();
+
+                ctx.font = "30px Arial";
+                ctx.fillStyle = "green";
+                ctx.translate(1250, 50);
+                ctx.scale(-1, 1);
+                ctx.fillText("Hand Detected!", 0, 0);
+                ctx.restore();
+
+            }else{
+                ctx.beginPath();
+                ctx.save();
+
+                ctx.font = "30px Arial";
+                ctx.fillStyle = "red";
+                ctx.translate(1250, 50);
+                ctx.scale(-1, 1);
+                ctx.fillText("Finding Hands...", 0, 0);
+                ctx.restore();
             }
-
-            ctx.beginPath();
-            ctx.save();
-
-            ctx.font = "30px Arial";
-            ctx.fillStyle = "green";
-            ctx.translate(1250, 50);
-            ctx.scale(-1, 1);
-            ctx.fillText("Hand Detected!", 0, 0);
-            ctx.restore();
-
-            //Loop through fingers (connect the point)
-            
-
-        }else{
-            ctx.beginPath();
-            ctx.save();
-
-            ctx.font = "30px Arial";
-            ctx.fillStyle = "red";
-            ctx.translate(1250, 50);
-            ctx.scale(-1, 1);
-            ctx.fillText("Finding Hand...", 0, 0);
-            ctx.restore();
         }
-        
 
-        
-        
-        
-            
         //Refresh
         requestAnimationFrame(handPoseFrame);
     }
