a46d59242aaef6f49ebd0090dd38e8ad8ad6f6bf
lky1020 <57024183+lky1020@users.noreply.github.com>
2021-07-08 20:41:51 +0800

    Add Try Catch to prevent WebXR error

--- src/background/webxr.js
+++ src/background/webxr.js
@@ -5,27 +5,33 @@ chrome.runtime.onConnect.addListener(port => {
   // @TODO: release connection when disconnected
 
   port.onMessage.addListener((message, sender, reply) => {
-    const tabId = message.tabId !== undefined ? message.tabId : sender.sender.tab.id;
+    try{
+        const tabId = message.tabId !== undefined ? message.tabId : sender.sender.tab.id;
 
-    if (connections[tabId] === undefined) {
-      connections[tabId] = {};
+        if (connections[tabId] === undefined) {
+            connections[tabId] = {};
+          }
+      
+          portMap = connections[tabId];
+      
+          portMap[port.name] = port;
+      
+          // transfer message between panel and webxrContentScript of the same tab
+      
+          if (port.name === 'webxrDevTools') {
+            if (portMap.webxrContentScript) {
+              portMap.webxrContentScript.postMessage(message);
+            }
+          }
+          if (port.name === 'webxrContentScript') {
+            if (portMap.panel) {
+              portMap.panel.postMessage(message);
+            }
+          }
+
+    }catch (e){
+        // The error can be ignore
     }
-
-    portMap = connections[tabId];
-
-    portMap[port.name] = port;
-
-    // transfer message between panel and webxrContentScript of the same tab
-
-    if (port.name === 'webxrDevTools') {
-      if (portMap.webxrContentScript) {
-        portMap.webxrContentScript.postMessage(message);
-      }
-    }
-    if (port.name === 'webxrContentScript') {
-      if (portMap.panel) {
-        portMap.panel.postMessage(message);
-      }
-    }
+    
   });
 });
