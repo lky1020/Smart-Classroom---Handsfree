6aa617b9426a3130abd7a849a13ac5d7cd61cfd0
lky1020 <57024183+lky1020@users.noreply.github.com>
2021-07-09 08:31:48 +0800

    Format Background.js

--- src/background/background.js
+++ src/background/background.js
@@ -14,65 +14,67 @@ const SPREADSHEET_ID = '1uzCzvLpFu9xeDTvPsot8AS-UmbaabsO4pf6sGDqsP-k';
 const SPREADSHEET_TAB_NAME = 'main';
 
 function onGAPILoad() {
-  gapi.client.init({
-    // Don't pass client nor scope as these will init auth2, which we don't want
-    apiKey: API_KEY,
-    discoveryDocs: DISCOVERY_DOCS,
+    gapi.client.init({
+        // Don't pass client nor scope as these will init auth2, which we don't want
+        apiKey: API_KEY,
+        discoveryDocs: DISCOVERY_DOCS,
 
-  }).then(function () {
-    console.log('gapi initialized')
+    }).then(function () {
+        console.log('gapi initialized')
 
-    chrome.identity.getAuthToken({interactive: true}, function(token) {
-        
-        gapi.auth.setToken({
-          'access_token': token,
-        });
-        
-        gapi.client.sheets.spreadsheets.values.get({
-            spreadsheetId: SPREADSHEET_ID,
-            range: SPREADSHEET_TAB_NAME,
-        }).then(function(response) {
+        chrome.identity.getAuthToken({ interactive: true }, function (token) {
 
-            console.log(`Got ${response.result.values.length} rows back`)
+            gapi.auth.setToken({
+                'access_token': token,
+            });
 
-        });
-    })
+            gapi.client.sheets.spreadsheets.values.get({
+                spreadsheetId: SPREADSHEET_ID,
+                range: SPREADSHEET_TAB_NAME,
+            }).then(function (response) {
 
-  }, function(error) {
-    console.log('error', error)
+                console.log(`Got ${response.result.values.length} rows back`)
 
-  });
+            });
+        })
+
+    }, function (error) {
+        console.log('error', error)
+
+    });
 }
 
 chrome.extension.onMessage.addListener(
-    function(request, sender, sendResponse) {
-      // Get the token
-      chrome.identity.getAuthToken({interactive: true}, function(token) {
-        // Set GAPI auth token
-        gapi.auth.setToken({
-          'access_token': token,
-        });
-  
-        const body = {values: [[
-          new Date(), // Timestamp
-          request.title, // Page title
-          request.url, // Page URl
-        ]]};
-  
-        // Append values to the spreadsheet
-        gapi.client.sheets.spreadsheets.values.append({
-          spreadsheetId: SPREADSHEET_ID,
-          range: SPREADSHEET_TAB_NAME,
-          valueInputOption: 'USER_ENTERED',
-          resource: body
-        }).then((response) => {
-          // On success
-          console.log(`${response.result.updates.updatedCells} cells appended.`)
-          sendResponse({success: true});
-        });
-      })
-  
-      // Wait for response
-      return true;
+    function (request, sender, sendResponse) {
+        // Get the token
+        chrome.identity.getAuthToken({ interactive: true }, function (token) {
+            // Set GAPI auth token
+            gapi.auth.setToken({
+                'access_token': token,
+            });
+
+            const body = {
+                values: [[
+                    new Date(), // Timestamp
+                    request.title, // Page title
+                    request.url, // Page URl
+                ]]
+            };
+
+            // Append values to the spreadsheet
+            gapi.client.sheets.spreadsheets.values.append({
+                spreadsheetId: SPREADSHEET_ID,
+                range: SPREADSHEET_TAB_NAME,
+                valueInputOption: 'USER_ENTERED',
+                resource: body
+            }).then((response) => {
+                // On success
+                console.log(`${response.result.updates.updatedCells} cells appended.`)
+                sendResponse({ success: true });
+            });
+        })
+
+        // Wait for response
+        return true;
     }
-  );
+);
